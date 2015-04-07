1b1008d57c53641c259c30998ed354e34510fe77
maryiv <admin@admins-mbp.d.office.onix.ua>
2015-04-07 12:04:28 +0300

    Init bower; jqury and bootstrap

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 var express = require('express');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var path = require('path');
 var config = require('config');
-var favicon = require('serve-favicon');
+var logger = require('morgan');
 var log = require('libs/log')(module);
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
+var MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
+
+var url = 'mongodb://localhost:27017/gallary';
+
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    log.info("Connected correctly to server");
+    db.close();
+});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(logger('dev'));
 app.listen(config.get('port'), function() {
     log.info('Welcome to Demo application on the PORT '+config.get('port'));
 });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser(config.get('crypKey')));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', routes);
 app.use('/users', users);
 
